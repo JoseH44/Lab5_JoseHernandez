e74976343a2628e944fd9de03b83c8ffe51f40fe
Jose <jrdjhc45@gmail.com>
2019-11-15 13:59:57 -0600

    creacion de la ultima clase llamada clan

---
+++ Lab5_JoseHernandez/src/lab5_josehernandez/Clan.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab5_josehernandez;
+
+import java.util.Date;
+
+/**
+ *
+ * @author jrdjh
+ */
+public class Clan {
+
+    private String nombre;
+    private Date fecha_creacion;
+    private String usuario_lider;
+    private String tipo;
+
+    public Clan() {
+    }
+
+    public Clan(String nombre, Date fecha_creacion, String usuario_lider, String tipo) {
+        this.nombre = nombre;
+        this.fecha_creacion = fecha_creacion;
+        this.usuario_lider = usuario_lider;
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getFecha_creacion() {
+        return fecha_creacion;
+    }
+
+    public void setFecha_creacion(Date fecha_creacion) {
+        this.fecha_creacion = fecha_creacion;
+    }
+
+    public String getUsuario_lider() {
+        return usuario_lider;
+    }
+
+    public void setUsuario_lider(String usuario_lider) {
+        this.usuario_lider = usuario_lider;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+
+}
